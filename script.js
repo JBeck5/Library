836a08abb435e20f492e19950961157c9d41fdbc
JBeck5 <73002731+JBeck5@users.noreply.github.com>
2025-04-14 13:28:45 -0700

    added remove function to card button

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-var modal = document.getElementById("newBookModal");
-var btn = document.getElementById("addBookBtn");
-var span = document.getElementsByClassName("close")[0];
-var addBookForm = document.getElementById("newBookForm");
-
-
+const modal = document.getElementById("newBookModal");
+const btn = document.getElementById("addBookBtn");
+const span = document.getElementsByClassName("close")[0];
+const addBookForm = document.getElementById("newBookForm");
+const bookShelf = document.querySelector(".shelf");
 
 btn.addEventListener("click", function() {
     modal.style.display = "block";
@@ -11,6 +10,7 @@ btn.addEventListener("click", function() {
 
 span.addEventListener("click", function() {
     modal.style.display = "none";
+    addBookForm.reset();
 })
 
 window.onclick = function(event) {
@@ -19,9 +19,10 @@ window.onclick = function(event) {
     }
 }
 
-document.getElementById("newBookForm").addEventListener("submit", (event) => {
+addBookForm.addEventListener("submit", (event) => {
     event.preventDefault();
     addBookToLibrary();
+    addBookForm.reset();
     modal.style.display = "none";
 })
 
@@ -41,10 +42,77 @@ function Book(title, author, pages, isRead) {
 function addBookToLibrary() {
   // take params, create a book then store it in the array
   // create new div for each newly created book?
-  let title = document.getElementById("title");
-  let author = document.getElementById("author");
-  let pages = document.getElementById("pages");
-  let isRead = document.getElementById("isRead");
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let isRead = document.getElementById("isRead").value;
   let newBook = new Book(title, author, pages, isRead);
-  console.log(newBook);
+  myLibrary.push(newBook);
+  displayLibrary();
+}
+
+function createCard(book) {
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book-card');
+
+    const title = document.createElement("div");
+    const titleText = document.createTextNode(`Title: ${book.title}.`);
+    title.appendChild(titleText);
+    bookCard.appendChild(title);
+
+    const author = document.createElement("div");
+    const authorText = document.createTextNode(`Title: ${book.author}.`);
+    author.appendChild(authorText);
+    bookCard.appendChild(author);
+
+    const pages = document.createElement("div");
+    const pagesText = document.createTextNode(`Title: ${book.pages}.`);
+    pages.appendChild(pagesText);
+    bookCard.appendChild(pages);
+
+    const isRead = document.createElement("div");
+    const isReadText = document.createTextNode(`${book.isRead ? 'Read.' : 'Not Read'}`);
+    isRead.appendChild(isReadText);
+    bookCard.appendChild(isRead);
+
+    const removeBtn = document.createElement('button');
+    removeBtn.classList.add('remove-button');
+    const removeBtnText = document.createTextNode(`Remove`);
+    removeBtn.appendChild(removeBtnText);
+    removeBtn.setAttribute('data-id', book.id);
+
+    bookCard.appendChild(removeBtn);
+
+    bookShelf.appendChild(bookCard);
+}
+
+function displayLibrary() {
+    bookShelf.innerHTML = " ";
+
+    myLibrary.forEach(book => {
+        createCard(book)
+    })
+
+    addRemoveListener();
+}
+
+function addRemoveListener() {
+    const removeButtons = document.querySelectorAll(".remove-button");
+    removeButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            const id = button.getAttribute("data-id");
+            removeBookFromLibrary(id);
+            displayLibrary(myLibrary);
+        })
+    })
+}
+
+function removeBookFromLibrary(bookId) {
+    //Finds location of selected book to remove
+    const bookIndex = myLibrary.findIndex(book => book.id === bookId);
+
+    //book check and removes from location
+    if (bookIndex != -1) {
+        myLibrary.splice(bookIndex, 1);
+    }
 }
